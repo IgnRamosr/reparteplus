9225a8df00da22efe70aaa43ef85372a44d03654
SEBASTIANTAPIAARAVENA <sebastiantapiaaravena@gmail.com>
2025-10-02 23:01:03 -0300

    codigo con fallas

--- app/DetalleGrupo.tsx
+++ app/DetalleGrupo.tsx
@@ -149,6 +149,7 @@ export default function DetalleGrupo() {
         style: 'destructive',
         onPress: () => setGastos(prev => prev.filter(g => g.id !== gastoId)),
       },
+
     ]);
   };
 
